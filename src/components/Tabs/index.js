d20cfda9570556f203066d02df8940e8e9ab4d35
Joao Lourencao <joao.lourencao@bitzar.solutions>
2020-10-14 16:12:47 -0300

    Tabs and central card

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,9 +1,34 @@
 import { func } from 'prop-types';
 import React from 'react';
-import { Container } from './styles';
+import { Container, TabsContainer, TabItem, TabText } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 export default function  Tabs() {
     return(
-        <Container/>
+        <Container>
+            <TabsContainer>
+                <TabItem>
+                    <Icon name="person-add" size={24} color="#FFF"/>
+                    <TabText>Indicar amigo</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="chat-bubble-outline" size={24} color="#FFF"/>
+                    <TabText>Cobrar</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="arrow-downward" size={24} color="#FFF"/>
+                    <TabText>Depositar</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="person-add" size={24} color="#FFF"/>
+                    <TabText>Tranferir</TabText>
+                </TabItem>
+                <TabItem>
+                    <Icon name="person-add" size={24} color="#FFF"/>
+                    <TabText>Bloquear cartão</TabText>
+                </TabItem>
+            </TabsContainer>
+        </Container>
     );
 }
